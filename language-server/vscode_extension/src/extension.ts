068e8f3f3461b3553cf2dfbaa554a0a790100a4f
Marcel Garus <github@marcelgarus.dev>
2022-11-23 11:02:11 +0100

    Use lower-level VSCode API

--- language-server/vscode_extension/src/extension.ts
+++ language-server/vscode_extension/src/extension.ts
@@ -3,37 +3,51 @@ import * as vs from 'vscode';
 export async function activate(context: vs.ExtensionContext) {
   console.log('Activated GraalVM Space Analyzer!');
 
-  let subscription = vs.languages.registerInlayHintsProvider('java', {
-    provideInlayHints: (
-      model,
-      range,
-      token
-    ): vs.ProviderResult<vs.InlayHint[]> => {
-      // TODO: Only show hints in the given range.
-      return new Promise(async (resolve) => {
-        const text = model.getText();
-        const hints: vs.InlayHint[] = [];
-        text.split('\n').forEach((text, index) => {
-          if (!text.includes('void ')) return;
+  context.subscriptions.push(
+    vs.window.onDidChangeActiveTextEditor(() => update())
+  );
 
-          hints.push(
-            new vs.InlayHint(
-              new vs.Position(index, text.length),
-              '⠀⠀⠀⠀⠀<- btw, this function results in 20 kB extra in the final binary',
-              vs.InlayHintKind.Type
-            )
-          );
-        });
-        resolve(hints);
-      });
-    },
-  });
+  if (vs.window.activeTextEditor) {
+    update();
+  }
+
   console.log('Registered hints provider.');
-
-  context.subscriptions.push(subscription);
 }
 
 export function deactivate(): Thenable<void> | undefined {
   // Do nothing. If we spawned a language server, it would have been stopped here.
   return undefined;
+}
+
+let decorationType = vs.window.createTextEditorDecorationType({});
+
+function update() {
+  console.log('Updating');
+
+  const editor = vs.window.activeTextEditor;
+  if (!editor) return;
+  const text = editor.document.getText();
+
+  type Item = vs.DecorationOptions & {
+    renderOptions: { after: { contentText: string } };
+  };
+  const hints: Item[] = [];
+  text.split('\n').forEach((line, index) => {
+    if (!line.includes('void ')) return;
+
+    hints.push({
+      range: new vs.Range(
+        new vs.Position(index, 0),
+        new vs.Position(index, line.length)
+      ),
+      renderOptions: {
+        after: {
+          contentText:
+            '⠀⠀⠀⠀⠀<- btw, this function results in 20 kB extra in the final binary',
+        },
+      },
+    });
+
+    editor.setDecorations(decorationType, hints);
+  });
 }
