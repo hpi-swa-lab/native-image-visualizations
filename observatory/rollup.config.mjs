96841078d03fc5733effa92b11fe15ac74208cbb
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2022-11-28 20:03:59 +0100

    Adds comments to options for better understanding & changes format to umd to resolve export statements

--- observatory/rollup.config.mjs
+++ observatory/rollup.config.mjs
@@ -3,21 +3,15 @@ import typescript  from '@rollup/plugin-typescript';
 
 export default [
     {
-        input: 'assets/js/dist/venn_diagram.js',
+        input: "assets/js/src/main.ts",
         output: {
-            sourcemap: true,
-            file: 'assets/js/dist/venn_diagram.min.js',
-            format: 'iife',
+            name: "Observatory",
+            file: "assets/js/dist/observatory.umd.js",
+            format: 'umd', // browser-friendly UMD build for both browsers and Node.js:
         },
-        plugins: [nodeResolve(), typescript({sourceMap: false})]
-    },
-    {
-        input: 'assets/js/dist/tree_viz.js',
-        output: {
-                sourcemap: true,
-                file: 'assets/js/dist/tree_viz.min.js',
-                format: 'iife',
-            },
-        plugins: [nodeResolve(), typescript({sourceMap: false})]
-    },
+        plugins: [
+            nodeResolve(), // so rollup can find node modules
+            typescript(),
+        ]
+    }
 ]
