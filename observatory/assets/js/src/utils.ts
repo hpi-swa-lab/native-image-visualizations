76c0ee033f3323ad2b235c396b08970e9badeab6
LucPrestin <l.prestin@gmx.de>
2022-11-30 15:30:21 +0100

    filter out generated code in hierarchy calculation

--- observatory/assets/js/src/utils.ts
+++ observatory/assets/js/src/utils.ts
@@ -8,3 +8,10 @@ export function randomColor(): string {
 
     return color
 }
+
+export function randomInteger(min: number, max: number): number {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
