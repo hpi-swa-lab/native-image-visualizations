c2ddad9c26543c4ac52d5f0769910abb366c2e1c
LucPrestin <l.prestin@gmx.de>
2022-12-14 15:55:23 +0100

    minor fixes

--- observatory/rollup.config.mjs
+++ observatory/rollup.config.mjs
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
