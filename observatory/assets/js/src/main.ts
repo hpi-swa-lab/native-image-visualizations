c292de2d0720209055877539e07f1aca7f6897c5
LucPrestin <l.prestin@gmx.de>
2023-01-04 13:36:01 +0100

    use file based report parsing

--- observatory/assets/js/src/main.ts
+++ observatory/assets/js/src/main.ts
@@ -1,8 +1,12 @@
 import HierarchyBubbles from './Visualizations/HierarchyBubbles'
 import VennVisualization from './Visualizations/VennVisualization'
 import TreeVisualization from './Visualizations/TreeVisualization'
-import { loadTextFile } from './BuildReportsParser'
-import { parseToCleanedPackageHierarchy, parseBuildReportToNodeWithSizeHierarchy } from './BuildReportsParser'
+import {
+    loadTextFile,
+    loadBuildReport,
+    parseToCleanedPackageHierarchy,
+    parseBuildReportToNodeWithSizeHierarchy
+} from './BuildReportsParser'
 
 export async function generateHierarchyBubbles(file: File): Promise<HierarchyBubbles> {
     const inputString = await loadTextFile(file)
@@ -24,17 +28,7 @@ export function generateTree() {
     tree.generate()
 }
 
-export function testBuildReportParser() {
-    let reports: Record<string, any>[] = [{
-        'name': 'com.oracle.test$testSomething$testSomethingElse.toString():String',
-        'size': 50
-    }, {
-        'name': 'com.oracle.test$testSomething$testSomethingElse.toInt():Int',
-        'size': 28
-    }, {
-        'name': 'com.oracle.test$testSomething.toString():String',
-        'size': 35
-    }]
-
-    return parseBuildReportToNodeWithSizeHierarchy(reports)
+export async function testBuildReportParser(file: File) {
+    const reportData = await loadBuildReport(file)
+    return parseBuildReportToNodeWithSizeHierarchy(reportData)
 }
