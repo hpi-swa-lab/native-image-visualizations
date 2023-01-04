cc05fbaa2cf38adfd9ca8ccfb001310f75f9e08d
LucPrestin <l.prestin@gmx.de>
2023-01-04 15:08:51 +0100

    remove double import

--- observatory/rollup.config.mjs
+++ observatory/rollup.config.mjs
@@ -1,4 +1,3 @@
-import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
