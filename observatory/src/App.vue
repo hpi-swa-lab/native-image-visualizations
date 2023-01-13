ddc00085265366170b1b016e170af856294ee41f
LucPrestin <l.prestin@gmx.de>
2023-01-13 14:40:19 +0100

    Add apps for the visualizations

--- observatory/src/App.vue
+++ observatory/src/App.vue
@@ -2,17 +2,29 @@
 import Sidebar from './components/Sidebar.vue'
 import { VisualizationType } from './ts/types/visualizationType';
 
+import HierarchyBubbles from './visualizations/HierarchyBubbles.vue'
+import SankeyTree from './visualizations/SankeyTree.vue'
+import TreeLine from './visualizations/TreeLine.vue'
+import Venn from './visualizations/Venn.vue'
+import ZoomableCausalityGraph from './visualizations/ZoomableCausalityGraph.vue'
 
-function changeVisualization(newVis: VisualizationType) {
-
-}
+let currentVis = VisualizationType.HierarchyBubbles
 
 </script>
 
 <template>
   <div>
     <Sidebar
-      @visualization-change="changeVisualization"
-    ></Sidebar>
+      @visualization-change="(newVis: VisualizationType) => {currentVis = newVis}"
+    >
+    <slot name="visualization-specific-options"/>
+  </Sidebar>
+  <div>
+    <HierarchyBubbles :v-if="currentVis === VisualizationType.HierarchyBubbles"/>
+    <SankeyTree :v-if="currentVis === VisualizationType.SankeyTree"/>
+    <TreeLine :v-if="currentVis === VisualizationType.TreeLine"/>
+    <Venn :v-if="currentVis === VisualizationType.Venn"/>
+    <ZoomableCausalityGraph :v-if="currentVis === VisualizationType.ZoomableCausalityGraph"/>
+  </div>
   </div>
 </template>
