cd0fcd8847121de2252d8cbd9cef8c6b54529183
LucPrestin <l.prestin@gmx.de>
2023-01-13 14:00:24 +0100

    switch to node module resolution to avoid linter issues due to conflicting module systems

--- observatory2/src/App.vue
+++ observatory2/src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { propsToAttrMap } from '@vue/shared';
 import Sidebar from './components/Sidebar.vue'
 import { VisualizationType } from './types/visualizationType';
 
+
+function changeVisualization(newVis: VisualizationType) {
+
+}
 
 </script>
 
 <template>
   <div>
     <Sidebar
-      @visualization-change="(newVis: VisualizationType) => {}"
+      @visualization-change="changeVisualization"
     ></Sidebar>
   </div>
 </template>
--- observatory2/src/components/Sidebar.vue
+++ observatory2/src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { VisualizationType } from '../types/visualizationType'
+import { VisualizationType } from '../types/visualizationType.ts'
 
 const emit = defineEmits(['visualization-change'])
 </script>
