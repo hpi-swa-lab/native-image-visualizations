90019571460768ac8125c28e26718e5280deea00
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-01-24 15:49:07 +0100

    Removes yet unused code

--- observatory/src/ts/SharedTypes/visualizationType.ts
+++
@@ -1,7 +0,0 @@
-export enum VisualizationType {
-    Venn,
-    SankeyTree,
-    TreeLine,
-    HierarchyBubbles,
-    ZoomableCausalityGraph
-}
--- observatory/tests/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from '@jest/globals'
-import * as utils from '../src/ts/utils'
-
-describe('utils module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(utils.sum(1, 2)).toBe(3)
-    })
-})
