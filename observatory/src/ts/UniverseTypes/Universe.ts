3165fa469af8efe5918e5f680b442108a5b7f60b
LucPrestin <l.prestin@gmx.de>
2023-01-25 16:33:09 +0100

    add interface definitions for visualizations

---
+++ observatory/src/ts/UniverseTypes/Universe.ts
@@ -0,0 +1,6 @@
+import { Node } from './Node'
+
+export interface Universe {
+    name: string,
+    root: Node
+}
--- observatory/src/ts/Visualizations/Visualization.ts
+++ observatory/src/ts/Visualizations/Visualization.ts
@@ -1,5 +1,21 @@
 import { Node } from '../UniverseTypes/Node'
+import { Universe } from '../UniverseTypes/Universe'
 
-export default interface Visualization {
-    visualize(node: Node): void
+/**
+ * Side Notes: 
+ * 
+ * - You are expected to get the initial data via the constructor. This cannot be modelled with interfaces.
+ * - You are also expected to do the visualization setup in the constructor.
+ */
+export interface Visualization {
+    selection: Node[]
+    setSelection(selection: Node[]): void
+}
+
+export interface UniverseVisualization extends Visualization{
+    setUniverse(universe: Universe): void
+}
+
+export interface MultiverseVisualization extends Visualization {
+    setMultiverse(multiverse: Universe[]): void
 }
