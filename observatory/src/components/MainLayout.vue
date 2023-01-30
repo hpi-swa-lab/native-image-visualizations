d69a51219bce4762656c8ccc3ab4eef3b53bed88
linaur <linaurban96@gmail.com>
2023-01-30 15:35:06 +0100

    encapsulate content of MainLayout in a div

--- observatory/src/components/MainLayout.vue
+++ observatory/src/components/MainLayout.vue
@@ -21,33 +21,35 @@ defineExpose({ EventType })
 </script>
 
 <template>
-    <aside class="w-64 z-1 h-full">
-        <div class="px-3 py-4 overflow-y-auto rounded bg-gray-50 space-y-4">
-            <h2>{{ title }}</h2>
-            <hr />
-
-            <div>
-                <button type="button" class="btn btn-primary btn-primary:hover">
-                    WIP: Data Manager
-                </button>
-            </div>
-            <hr />
-
-            <VisualizationNavigation
-                :selected="visualizationType"
-                @change-viz="$emit(EventType.CHANGE_VIZ, $event)"
-            ></VisualizationNavigation>
-            <hr />
-
-            <UniverseSelectionList></UniverseSelectionList>
-            <hr />
-
-            <ul class="space-y-2">
-                <slot name="controls"> Controls </slot>
-            </ul>
-        </div>
-    </aside>
     <div class="w-full h-full">
-        <slot />
+        <aside class="w-64 z-1 h-full">
+            <div class="px-3 py-4 overflow-y-auto rounded bg-gray-50 space-y-4">
+                <h2>{{ title }}</h2>
+                <hr />
+
+                <div>
+                    <button type="button" class="btn btn-primary btn-primary:hover">
+                        WIP: Data Manager
+                    </button>
+                </div>
+                <hr />
+
+                <VisualizationNavigation
+                    :selected="visualizationType"
+                    @change-viz="$emit(EventType.CHANGE_VIZ, $event)"
+                ></VisualizationNavigation>
+                <hr />
+
+                <UniverseSelectionList></UniverseSelectionList>
+                <hr />
+
+                <ul class="space-y-2">
+                    <slot name="controls"> Controls </slot>
+                </ul>
+            </div>
+        </aside>
+        <div class="w-full h-full">
+            <slot />
+        </div>
     </div>
 </template>
