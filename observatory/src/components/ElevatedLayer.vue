6aac02d9a324948c3dd85204daae87b53d105e93
LucPrestin <l.prestin@gmx.de>
2023-01-30 17:12:44 +0100

    add elevated layer component

---
+++ observatory/src/components/ElevatedLayer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+const props = defineProps({
+    level: {
+        type: Number,
+        default: 2
+    }
+})
+const shadowClass = computed(() => {
+    switch (props.level) {
+        case 1:
+            return 'drop-shadow-md'
+        case 2:
+            return 'drop-shadow-lg'
+        case 3:
+            return 'drop-shadow-xl'
+        case 4:
+            return 'drop-shadow-2xl'
+        default:
+            return 'drop-shadow-md'
+    }
+})
+</script>
+
+<template>
+    <div :class="`space-y-5 p-4 bg-white z-${props.level * 10} ${shadowClass}`">
+        <slot />
+    </div>
+</template>
