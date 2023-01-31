5fd5be9d89b424cd0e30fa9ebed4f270893ff02c
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-01-31 20:22:02 +0100

    Refactors merge function

--- observatory/src/ts/GraphOperations/TreeMerger.ts
+++ observatory/src/ts/GraphOperations/TreeMerger.ts
@@ -3,30 +3,19 @@ import { Node } from './../UniverseTypes/Node'
 
 export function mergeTrees(...trees: Node[]): Node {
     const mergedTree: Node = new Node('')
-
-    for (let i = 0; i < trees.length; i++) {
-        mergeNode(mergedTree, trees[i], i)
-    }
-
-    // mergedTree.occurencesIn = []
+    trees.forEach((tree, i) => mergeNode(mergedTree, trees[i], i))
     return mergedTree
 }
 
 function mergeNode(mergedTree: Node, node: Node, treeIndex: UniverseIndex) {
-    let found = false
-    for (const mergedChild of mergedTree.children) {
-        if (mergedChild.name !== node.name) continue
-        found = true
-        mergedChild.occurencesIn.push(treeIndex)
-        for (const ownChild of node.children) {
-            mergeNode(mergedChild, ownChild, treeIndex)
-        }
-        break
-    }
+    const matchingChild = mergedTree.children.find((mergedChild) => mergedChild.name === node.name)
 
-    if (!found) {
+    if (!matchingChild) {
         setOccurencesInRecursive(node, treeIndex)
         mergedTree.push(node)
+    } else {
+        matchingChild.occurencesIn.push(treeIndex)
+        node.children.forEach((ownChild) => mergeNode(matchingChild, ownChild, treeIndex))
     }
 }
 
