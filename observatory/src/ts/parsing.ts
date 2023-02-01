14ca89aee332ad2e048385fd03909804c75cdfdc
LucPrestin <l.prestin@gmx.de>
2023-02-01 12:22:32 +0100

    annotate stubs as todo

--- observatory/src/ts/parsing.ts
+++ observatory/src/ts/parsing.ts
@@ -1,9 +1,13 @@
 import { Node } from './UniverseTypes/Node'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function loadJson(file: File): Promise<object> {
+    // TODO: implement this
     return {}
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function parseReachabilityExport(reachabilityExport: object): Node {
-    return new Node('root', undefined, [], 0)
+    // TODO: implement this
+    return new Node('root', [], undefined, 0)
 }
