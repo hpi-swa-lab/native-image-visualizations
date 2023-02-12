31fabfcbf3719193bf7ecbab3be8db5d542c4c50
Lina Urban <lina.urban@student.hpi.de>
2023-02-12 15:26:08 +0100

    extract colors into tailwind.config.cjs

--- observatory/tailwind.config.cjs
+++ observatory/tailwind.config.cjs
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                diffRed: colors.red[400],
+                diffGreen: colors.lime[400],
+                diffBlue: colors.sky[300],
+                diffGray: colors.gray[400],
+            }
+        },
+        
     },
     plugins: []
 }
