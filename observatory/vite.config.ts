d95b5c1108944842e9aee6be5deac32e6335af3a
Lina Urban <lina.urban@student.hpi.de>
2023-02-13 15:08:39 +0100

    Revert "setup tailwind config referencing with vite"
    This reverts commit 0ec24e3c4a7eff687c02e6c25332a6322141e522.

--- observatory/vite.config.ts
+++ observatory/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -8,23 +7,5 @@ export default defineConfig({
     server: {
         port: 8080
     },
-    
-    resolve: {
-        alias: {
-          'tailwind-config': path.resolve(__dirname, './tailwind.config.cjs'),
-        },
-    },
-
-    build: {
-        commonjsOptions: {
-            include: ['tailwind.config.cjs', 'node_modules/**']
-        }
-    },
-
-    optimizeDeps: {
-        include: ['tailwind-config'],
-    },
-
-    plugins: [vue()],
-    
+    plugins: [vue()]
 })
