d88274cc25df13b1dda4ed0739aa1e1583f7e52c
Lina Urban <lina.urban@student.hpi.de>
2023-02-13 15:08:40 +0100

    Revert "extract colors into tailwind.config.cjs"
    This reverts commit 31fabfcbf3719193bf7ecbab3be8db5d542c4c50.

--- observatory/tailwind.config.cjs
+++ observatory/tailwind.config.cjs
@@ -1,18 +1,8 @@
-const colors = require('tailwindcss/colors')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {
-            colors: {
-                diffRed: colors.red[400],
-                diffGreen: colors.lime[400],
-                diffBlue: colors.sky[300],
-                diffGray: colors.gray[400],
-            }
-        },
-        
+        extend: {}
     },
     plugins: []
 }
