33049019a629048481aea4a1afeab4c1fb43f917
LucPrestin <l.prestin@gmx.de>
2023-02-13 15:21:39 +0100

    run autofix

--- observatory/src/ts/parsing.ts
+++ observatory/src/ts/parsing.ts
@@ -8,6 +8,6 @@ export async function loadJson(file: File): Promise<object> {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function parseReachabilityExport(reachabilityExport: object): Node {
-    // TODO: implement this, corresponding issue: [#36](https://github.com/hpi-swa-lab/MPWS2022RH1/issues/36) 
+    // TODO: implement this, corresponding issue: [#36](https://github.com/hpi-swa-lab/MPWS2022RH1/issues/36)
     return new Node('root', [], undefined, 0)
 }
