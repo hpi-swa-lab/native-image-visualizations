dde35c8af39dde80b0fa86a3413666065c1c24f7
linaur <linaurban96@gmail.com>
2023-02-15 13:57:19 +0100

    prettier-autofix

--- observatory/src/ts/enums/EventType.ts
+++ observatory/src/ts/enums/EventType.ts
@@ -7,5 +7,5 @@ export enum EventType {
     SELECTION_CHANGED = 'selection-changed',
     SHOW_UNMODIFIED_CHANGED = 'show-unmodified-changed',
     EXPAND_TREE = 'expand-tree',
-    CHANGED = 'changed',
+    CHANGED = 'changed'
 }
