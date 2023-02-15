382df6781dc87831ce9713b510bcad3a5b246d2c
LucPrestin <l.prestin@gmx.de>
2023-02-15 14:43:56 +0100

    add searching nodes by name

---
+++ observatory/src/ts/Math/filters.ts
@@ -0,0 +1,15 @@
+import { Node } from '../UniverseTypes/Node'
+
+export function findNodesWithName(name: string, root: Node): Node[] {
+    let result = [] as Node[]
+
+    if (root.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())) {
+        result.push(root)
+    }
+
+    root.children.forEach((child: Node) => {
+        result = result.concat(findNodesWithName(name, child))
+    })
+
+    return result
+}
