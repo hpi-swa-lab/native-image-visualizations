3712c80db45300ff2f96f46f9b88aaadc7c4a5e0
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-02-19 03:05:39 +0100

    adds flashing animation

--- observatory/tailwind.config.cjs
+++ observatory/tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            keyframes: {
+                flash: {
+                    '0%': { opacity: '0'},
+                    '100%': { opacity: '1'},
+                }
+            },
+            animation: {
+                'flash': 'flash 0.7s ease-out',
+            }
+        }
     },
     plugins: []
 }
