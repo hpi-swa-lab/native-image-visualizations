c75e7bc05f13de68dc4d3d437e0bf589ce77a2c9
linaur <linaurban96@gmail.com>
2023-02-22 15:20:11 +0100

    remove json ending from universe name

--- observatory/src/components/controls/DataManager.vue
+++ observatory/src/components/controls/DataManager.vue
@@ -30,12 +30,16 @@ function validateFileAndAddUniverseOnSuccess(file: File, universeName: string): 
         })
 }
 
+function trimJsonEnding(fileName: string) {
+  return fileName.split('.json')[0]
+}
+
 function addUniverses(event: Event) {
     const input = event.target as HTMLInputElement
     if (!input.files) return
 
     Array.from(input.files).forEach((file: File) =>
-        validateFileAndAddUniverseOnSuccess(file, file.name)
+        validateFileAndAddUniverseOnSuccess(file, trimJsonEnding(file.name))
     )
 
     input.value = ''
