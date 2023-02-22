cca7e13a53fdf674f9240e42b5a0334dd345aaab
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-02-22 15:36:28 +0100

    Ignores universe names when merging (#107)
    * Support no naming for module
    
    * Ignores first layer when merging universes

--- observatory/tests/data/forest.ts
+++ observatory/tests/data/forest.ts
@@ -28,38 +28,7 @@ export const forestFactory = () => ({
             ])
         ])
     ]),
-    overlappingTreeA: new Node('packageA', [
-        new Node('ClassA', [new Node('methodAA')]),
-        new Node('ClassB', [new Node('methodBA')])
-    ]),
-    overlappingTreeB: new Node('packageA', [
-        new Node('ClassA', [new Node('methodAC')]),
-        new Node('ClassC', [new Node('methodCA')])
-    ]),
-    overlappingTreeC: new Node('packageA', [
-        new Node('ClassA'),
-        new Node('ClassX'),
-        new Node('ClassY')
-    ]),
-    differentPackageTree: new Node('packageB', [
-        new Node('ClassA', [new Node('methodAA')]),
-        new Node('ClassX', [new Node('methodXA')]),
-        new Node('ClassY', [new Node('methodYA')])
-    ]),
-    overlappingImageA: new Node('packageA', [
-        new Node('ClassA', [new Leaf('methodAA', 10, [InitKind.BUILD_TIME])]),
-        new Node('ClassB', [new Leaf('methodBA', 10, [InitKind.BUILD_TIME])])
-    ]),
-    overlappingImageB: new Node('packageA', [
-        new Node('ClassA', [new Leaf('methodAC', 15, [InitKind.BUILD_TIME])]),
-        new Node('ClassC', [new Leaf('methodCA', 20, [InitKind.BUILD_TIME])])
-    ]),
-    overlappingImageC: new Node('packageA', [
-        new Node('ClassA', [new Leaf('methodAC', 15, [InitKind.BUILD_TIME])]),
-        new Node('ClassX', [new Leaf('methodXA', 30, [InitKind.BUILD_TIME])]),
-        new Node('ClassY', [new Leaf('methodYA', 200, [InitKind.BUILD_TIME])])
-    ]),
-    duplicatedNames: new Node('module', [
+    duplicatedNames: new Node('anotherModule', [
         new Node('packageA', [
             new Node('ClassA', [new Leaf('methodA', 10, [InitKind.BUILD_TIME])]),
             new Node('ClassB', [
@@ -73,6 +42,47 @@ export const forestFactory = () => ({
                 new Leaf('methodB', 0, [InitKind.BUILD_TIME]),
                 new Leaf('methodC', 10, [InitKind.BUILD_TIME])
             ])
+        ])
+    ]),
+    overlappingTreeA: new Node('universe', [
+        new Node('packageA', [
+            new Node('ClassA', [new Node('methodAA')]),
+            new Node('ClassB', [new Node('methodBA')])
+        ])
+    ]),
+    overlappingTreeB: new Node('universe', [
+        new Node('packageA', [
+            new Node('ClassA', [new Node('methodAC')]),
+            new Node('ClassC', [new Node('methodCA')])
+        ])
+    ]),
+    overlappingTreeC: new Node('universe', [
+        new Node('packageA', [new Node('ClassA'), new Node('ClassX'), new Node('ClassY')])
+    ]),
+    differentPackageTree: new Node('universe', [
+        new Node('packageB', [
+            new Node('ClassA', [new Node('methodAA')]),
+            new Node('ClassX', [new Node('methodXA')]),
+            new Node('ClassY', [new Node('methodYA')])
+        ])
+    ]),
+    overlappingImageA: new Node('universe', [
+        new Node('packageA', [
+            new Node('ClassA', [new Leaf('methodAA', 10, [InitKind.BUILD_TIME])]),
+            new Node('ClassB', [new Leaf('methodBA', 10, [InitKind.BUILD_TIME])])
+        ])
+    ]),
+    overlappingImageB: new Node('universe', [
+        new Node('packageA', [
+            new Node('ClassA', [new Leaf('methodAC', 15, [InitKind.BUILD_TIME])]),
+            new Node('ClassC', [new Leaf('methodCA', 20, [InitKind.BUILD_TIME])])
+        ])
+    ]),
+    overlappingImageC: new Node('universe', [
+        new Node('packageA', [
+            new Node('ClassA', [new Leaf('methodAC', 15, [InitKind.BUILD_TIME])]),
+            new Node('ClassX', [new Leaf('methodXA', 30, [InitKind.BUILD_TIME])]),
+            new Node('ClassY', [new Leaf('methodYA', 200, [InitKind.BUILD_TIME])])
         ])
     ])
 })
