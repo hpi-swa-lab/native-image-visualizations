c0bfe53ad58db274bcc1989cc84fa022df39479f
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-03-03 00:40:13 +0100

    Basic version

--- observatory/src/components/visualizations/VennSets.vue
+++ observatory/src/components/visualizations/VennSets.vue
@@ -7,11 +7,15 @@ import { globalConfigStore } from '../../ts/stores'
 import { VennSets } from '../../ts/Visualizations/VennSets'
 import { Multiverse } from '../../ts/UniverseTypes/Multiverse'
 import { Node } from '../../ts/UniverseTypes/Node'
+import ToolTip from '../controls/ToolTip.vue'
 
 const emit = defineEmits([EventType.CHANGE])
 const store = globalConfigStore()
 
-const container = ref<HTMLDivElement>()
+const tooltipContent = ref<string>('')
+const tooltipShow = ref<boolean>(false)
+const tooltipX = ref<number>(0)
+const tooltipY = ref<number>(0)
 
 const multiverse = computed(() => store.multiverse)
 const currentLayer = computed(() => store.currentLayer)
@@ -20,10 +24,25 @@ const selection = computed(() => store.selections)
 
 let visualization: VennSets
 
+function showToolTip(content: string) {
+    tooltipContent.value = content;
+    tooltipShow.value = true;
+}
+
+function updateToolTipPosition(x: number, y: number) {
+    tooltipX.value = x
+    tooltipY.value = y
+}
+
+function hideToolTip() {
+    tooltipShow.value = false
+}
+
+
 // The reason for using as <...> is that the store saves Proxy Types of the objects
 
 onMounted(() => {
-    visualization = new VennSets('#viz-container', store.currentLayer, store.colorScheme)
+    visualization = new VennSets('#viz-container', store.currentLayer, store.colorScheme, {showToolTip, updateToolTipPosition, hideToolTip})
     visualization.setMultiverse(store.multiverse as Multiverse)
 })
 
@@ -55,6 +74,7 @@ watch(
         :component-type="SwappableComponentType.VennSets"
         @change-page="(componentType: SwappableComponentType) => emit(EventType.CHANGE, componentType)"
     >
+        <ToolTip ref="tooltip" :show="tooltipShow" :content="tooltipContent" :x="tooltipX" :y="tooltipY"></ToolTip>
         <div id="viz-container" ref="container" class="w-full h-full"></div>
     </MainLayout>
 </template>
