37503aa72969afe43eec5bc689cfca4ec281f061
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-03-05 23:40:10 +0100

    Update Size.ts

--- observatory/src/ts/SharedTypes/Size.ts
+++ observatory/src/ts/SharedTypes/Size.ts
@@ -6,15 +6,13 @@ const BYTES_IN_GB = BYTES_IN_MB * 1024
 const BYTES_IN_TB = BYTES_IN_GB * 1024
 
 export function formatBytes(bytes: Bytes, cutOffAt = 2): string {
-    if (bytes < BYTES_IN_KB) {
-        return bytes + ' bytes'
-    } else if (bytes < BYTES_IN_MB) {
-        return (bytes / BYTES_IN_KB).toFixed(cutOffAt) + ' KB'
-    } else if (bytes < BYTES_IN_GB) {
-        return (bytes / BYTES_IN_MB).toFixed(cutOffAt) + ' MB'
-    } else if (bytes < BYTES_IN_TB) {
-        return (bytes / BYTES_IN_GB).toFixed(cutOffAt) + ' GB'
-    } else {
-        return (bytes / BYTES_IN_TB).toFixed(cutOffAt) + ' TB'
-    }
+    if (bytes < BYTES_IN_KB) return bytes + ' bytes'
+
+    if (bytes < BYTES_IN_MB) return (bytes / BYTES_IN_KB).toFixed(cutOffAt) + ' KB'
+
+    if (bytes < BYTES_IN_GB) return (bytes / BYTES_IN_MB).toFixed(cutOffAt) + ' MB'
+
+    if (bytes < BYTES_IN_TB) return (bytes / BYTES_IN_GB).toFixed(cutOffAt) + ' GB'
+
+    return (bytes / BYTES_IN_TB).toFixed(cutOffAt) + ' TB'
 }
