dfd9ca0fecbc01e46e3e58bcdb03df9c0c19ec53
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-03-06 15:12:10 +0100

    replaces space with thin space

--- observatory/src/ts/SharedTypes/Size.ts
+++ observatory/src/ts/SharedTypes/Size.ts
@@ -6,13 +6,13 @@ const BYTES_IN_GB = BYTES_IN_MB * 1000
 const BYTES_IN_TB = BYTES_IN_GB * 1000
 
 export function formatBytes(bytes: Bytes, cutOffAt = 2): string {
-    if (bytes < BYTES_IN_KB) return bytes + ' Bytes'
+    if (bytes < BYTES_IN_KB) return bytes + ' Bytes'
 
-    if (bytes < BYTES_IN_MB) return (bytes / BYTES_IN_KB).toFixed(cutOffAt) + ' KB'
+    if (bytes < BYTES_IN_MB) return (bytes / BYTES_IN_KB).toFixed(cutOffAt) + ' KB'
 
-    if (bytes < BYTES_IN_GB) return (bytes / BYTES_IN_MB).toFixed(cutOffAt) + ' MB'
+    if (bytes < BYTES_IN_GB) return (bytes / BYTES_IN_MB).toFixed(cutOffAt) + ' MB'
 
-    if (bytes < BYTES_IN_TB) return (bytes / BYTES_IN_GB).toFixed(cutOffAt) + ' GB'
+    if (bytes < BYTES_IN_TB) return (bytes / BYTES_IN_GB).toFixed(cutOffAt) + ' GB'
 
     return (bytes / BYTES_IN_TB).toFixed(cutOffAt) + ' TB'
 }
