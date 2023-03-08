380783e6428369ea72afb5b7fb23024d71430c7a
LucPrestin <l.prestin@gmx.de>
2023-03-08 16:05:52 +0100

    fix eslint problems with empty loadings

--- observatory/src/ts/stores/causalityGraphStore.ts
+++ observatory/src/ts/stores/causalityGraphStore.ts
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * Reason for disabling: currently, these methods do nothing,
+ * as the treeLineConfig does not have any state. However,
+ * these methods should still exist for uniformity. Disabling the checks
+ * for only line 16 did not work, as eslint then complained about the
+ * eslint-disable calls to be too long a line of code.
+ */
+
 import { defineStore } from 'pinia'
 
 export type CausalityGraphConfig = Record<string, unknown>
--- observatory/src/ts/stores/treeLineStore.ts
+++ observatory/src/ts/stores/treeLineStore.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * Reason for disabling: currently, these methods do nothing,
+ * as the treeLineConfig does not have any state. However,
+ * these methods should still exist for uniformity. Disabling the checks
+ * for only line 16 did not work, as eslint then complained about the
+ * eslint-disable calls to be too long a line of code.
+ */
+
 import { defineStore } from 'pinia'
 
 export type TreeLineConfig = Record<string, unknown>
 
 export const useTreeLineStore = defineStore('treeLineConfig', {
     actions: {
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
         loadExportDict(config: TreeLineConfig): void {},
         toExportDict(): TreeLineConfig {
             return {}
