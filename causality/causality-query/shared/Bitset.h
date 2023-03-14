c6f2f26203d94c52f5ef481b580da8685a3b6b0e
ChristophTF <77843385+ChristophTF@users.noreply.github.com>
2023-03-14 18:06:30 +0100

    Causality Query (#148)
    * Add subuniverse-reachability with custom graal
    
    * real/execute-ni.py: Usable tool with make example
    
    * WIP: Causality
    
    * WIP: shortest-path to reachable method
    
    * Virtual Invokes as special case of containing method
    
    * Introduce Saturation in BFS
    
    * +Performance
    
    * Reachability Shortest-Path with Saturation
    
    * purge-and-BFS all methods
    
    * Don't print BFS-dist
    
    * WIP: Class based bruteforce_purge
    
    * WIP: Transitive reduction on purge graph
    
    * Quick-and-Dirty WASM Demo
    
    * Usability-Improvements
    
    * Split code in multiple files
    
    * Bugfix: Don't pass moved object
    
    * Move BFS to analysis.h
    
    * Incorporate BFS optimizations
    
    * Add parallel reachability window
    
    * Enable '*' operator on reachability query
    
    * Hide class name retrieval behind "#if LOG"
    
    * Remove class transformation exception rules that have become unnecessary
    
    * Calculate dominator graph
    
    * Fix reachability virtual invocation with saturation
    
    * Use same reachability input syntax for console as for web version
    
    * Fix empty worklist bug in reachability.h
    
    * Return an empty string instead of null if input is valid empty string
    
    * WIP: Incremental BFS using Konrad Letz's inverted Segtree trick
    
    * Make Incremental BFS operation reversible
    
    * Use vector instead of list for saturation_uses_by_filter
    
    * reuse filter.count()
    
    * Mo' moves
    
    * Remove dead comments
    
    * Subset of purged methods parametrizable
    
    * Extract result callback
    
    * Verallgemeinerung auf disjunkte Teilmengen: Tut der Performance ein bisschen weh (<10%)
    
    * Expose batch-purging to wasm
    
    * WIP: Trying to get incremental-bfs running on wasm
    
    * Incremental-BFS running on wasm
    
    * Weighted halving
    
    * Enable basic Exception support
    
    * Remove -fexceptions again for performance
    
    * Forgive if saturated type does not come from allInstantiated
    
    * Make causality_graph-wasm run with spring-petclinic order of magnitude in data
    
    * Callback-based result reporting for less coupling
    
    * Use node_ids instead of strings for js-wasm interface
    
    * Implement an equivalence test for the redundant typeflow elimination functionality
    
    * WIP: Reordering Typeflow-subgraph-Optimization
    
    * Correct search for saturation reason in reachability trace
    
    * Full purge matrix dump for regression testing
    
    * WIP: Renaming of method_visited
    
    * Custom datatype for method_history
    
    * Only allow typeflow 0 as root
    
    * Generalize bfs-incremental to tree structure
    
    * simulatePurgesBatched uses hierarchical inputs
    
    * Show reachable-making hyperpath
    
    * Remove duplicate edges
    
    * Model optimization must be called explicitly
    
    * Faster typeflow graph optimization
    
    * Do not read declaring_types.bin, typeflows.txt, methods.txt, types.txt in Causality Graph simulator
    
    * Show flowing type on tooltip of typeflow-caused edges
    
    * Purge trivial typestates from memory
    
    * Fix purge trivial typestates
    
    * Object-oriented API to wasm module
    
    * Custom bindings got it working
    
    * Extract correctness assertions logic from bfs_incremental_rec(...)
    
    * Rewrite Incremental-BFS from recursive to iterative
    
    * IncrementalBfs gets its own class
    
    * Encapsulate necessary preparation work into IncrementalBfs
    
    * Exorcise State from BFS class (into Adjacency)
    
    * Make interface of IncrementalBfs to observatory enumeration-based
    
    * Include boost::dynamic_bitset
    
    * List CMake as a requirement
    
    * Remove checked-in boost code
    
    * Who needs boost anyway?
    
    * Prepare for "graal" as submodule in top-level-directory
    
    * Increase stack size

---
+++ causality/causality-query/shared/Bitset.h
@@ -0,0 +1,93 @@
+#ifndef CAUSALITY_GRAPH_BITSET_H
+#define CAUSALITY_GRAPH_BITSET_H
+
+#include <vector>
+#include <iostream>
+#include <bit>
+
+class Bitset
+{
+    using block_t = uint64_t;
+    static constexpr size_t bits_per_block = sizeof(block_t) * 8;
+
+    std::vector<block_t> blocks;
+    size_t len;
+    size_t _count;
+
+public:
+    Bitset(size_t len) : len(len), blocks((len + (bits_per_block - 1)) / bits_per_block)
+    {}
+
+    void fill(std::istream& src)
+    {
+        src.read((char*)blocks.data(), (len + 7) / 8);
+
+        _count = 0;
+        for(block_t block : blocks)
+            _count += std::popcount(block);
+    }
+
+    bool operator[](size_t i) const
+    {
+        return (blocks[i / bits_per_block] & (block_t(1) << (i % bits_per_block))) != 0;
+    }
+
+    [[nodiscard]] bool is_superset(const Bitset& other) const
+    {
+        if(other.len != this->len)
+            exit(1);
+
+        bool res = true;
+        for(size_t i = 0; i < blocks.size(); i++)
+        {
+            bool block_is_superset = (~blocks[i] & other.blocks[i]) == 0;
+            res &= block_is_superset;
+            // Not returning eagerly so that the compiler can assume the memory
+            // referenced in all future iterations as valid.
+        }
+
+        return res;
+    }
+
+    [[nodiscard]] size_t count() const
+    {
+        return _count;
+    }
+
+    [[nodiscard]] size_t first() const
+    {
+        for(size_t i = 0; i < blocks.size(); i++)
+            if(blocks[i] != 0)
+                return i * bits_per_block + std::countr_zero(blocks[i]);
+
+        return std::numeric_limits<size_t>::max();
+    }
+
+    [[nodiscard]] size_t next(size_t pos) const
+    {
+        block_t already_started = blocks[pos / bits_per_block];
+        already_started >>= (pos % bits_per_block) + 1;
+
+        if(already_started)
+            return pos + 1 + std::countr_zero(already_started);
+
+        for(size_t i = pos / bits_per_block + 1; i < blocks.size(); i++)
+            if(blocks[i] != 0)
+                return i * bits_per_block + std::countr_zero(blocks[i]);
+
+        return std::numeric_limits<size_t>::max();
+    }
+
+    size_t size() const
+    {
+        return len;
+    }
+
+    bool operator==(const Bitset& other) const
+    {
+        // Based on the assumption that the unused leftmost bits are always zero
+        return blocks == other.blocks;
+    }
+};
+
+#endif //CAUSALITY_GRAPH_BITSET_H
