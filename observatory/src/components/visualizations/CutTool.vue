36c812d1af10f62bdd219d5f691aca5ed3762279
Christoph Blumschein <christoph.blumschein@student.hpi.uni-potsdam.de>
2023-03-15 00:35:37 +0100

    Improved DetailView

--- observatory/src/components/visualizations/CutTool.vue
+++ observatory/src/components/visualizations/CutTool.vue
@@ -25,15 +25,21 @@ watch(multiverse, (newMultiverse) => {
     const universes = newMultiverse.sources
     if (universes.length === 1) visualization?.setUniverse(toRaw(universes[0]))
 })
+
+function detailViewClose(): void {
+    visualization?.closeDetailView()
+}
 </script>
+
+<style src="./CutTool.css" />
 
 <template>
     <MainLayout title="Cut Tool">
         <div id="cut-tool-root" ref="container">
             <!-- eslint-disable -->
-            <component is="style" scoped>
-                {{ styleContent }}
-            </component>
+            <!--            <component is="style" scoped>-->
+            <!--                {{ styleContent }}-->
+            <!--            </component>-->
             <!-- eslint-enable -->
             <div id="loading-panel" class="fullscreen" hidden>
                 <div class="center">Causality Graph is being parsed...</div>
@@ -44,13 +50,25 @@ watch(multiverse, (newMultiverse) => {
                     <span><b>Cut Overview:</b></span>
                     <div id="cut-overview-root"></div>
                 </div>
-                <div id="detail-div" hidden>
-                    <svg id="detail-svg" class="h-full w-full">
-                        <g id="chartpanel">
-                            <rect class="h-full w-full opacity-0" />
-                            <g id="chart"></g>
-                        </g>
-                    </svg>
+                <div id="detail-div" hidden class="outer-ne-resize">
+                    <div class="inner-ne-resize">
+                        <button
+                            class="absolute right-[10px] z-10 btn bg-gray-50 mt-2 ml-2 hover:bg-gray-200 shadow-md"
+                        >
+                            <font-awesome-icon
+                                icon="xmark"
+                                @click="detailViewClose"
+                            />
+                        </button>
+                        <span style="position: absolute; padding: 10px; background: white"
+                            ><b>Detail-view:</b></span
+                        >
+                        <svg id="detail-svg" class="h-full w-full">
+                            <g id="chartpanel">
+                                <g id="chart"></g>
+                            </g>
+                        </svg>
+                    </div>
                 </div>
                 <div id="imageview-div">
                     <span><b>In-Image Overview:</b></span>
