bc24d82b9d0dc6d739ccd4599db6bca7f7dc2b3e
LucPrestin <40767277+LucPrestin@users.noreply.github.com>
2023-03-20 22:40:24 +0100

    Slider rework (#166)
    * add helper for getting all layers with names
    
    * use layerlist for styling the granularity selection
    
    * Implement PR feedback
    
    Co-authored-by: Lina Urban <linaurban96@gmail.com>
    
    * make sidepanel wider to fix layout
    
    * Make spacing more uniform
    
    Co-authored-by: Lina Urban <linaurban96@gmail.com>
    
    * rename method to make the intent clear
    
    * Update observatory/src/components/controls/VennControls.vue
    
    Co-authored-by: Lina Urban <linaurban96@gmail.com>
    
    * rwork layer calculation
    
    Co-authored-by: Joana Bergsiek <33000454+JoeAtHPI@users.noreply.github.com>
    
    * remove unneeded interface
    
    Co-authored-by: Joana Bergsiek <33000454+JoeAtHPI@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lina Urban <linaurban96@gmail.com>
    Co-authored-by: Joana Bergsiek <33000454+JoeAtHPI@users.noreply.github.com>

--- observatory/src/components/controls/VennControls.vue
+++ observatory/src/components/controls/VennControls.vue
@@ -9,19 +9,21 @@ const vennStore = useVennStore()
 <template>
     <div id="venn-controls" class="input-container settings-container space-y-4">
         <LayerSelection />
-        <label class="ml-1"> Sorting: </label>
-        <template v-for="(order, index) in Object.values(SortingOrder)" :key="index">
-            <div class="relative block">
-                <input
-                    :id="order"
-                    name="sorting-options"
-                    :value="order"
-                    type="radio"
-                    :checked="vennStore.isSortingOrder(order)"
-                    @change="vennStore.setSortingOrder(order)"
-                />
-                <label :for="order" class="ml-1"> {{ order }} </label>
-            </div>
-        </template>
+        <div class="space-y-4">
+            <label class="ml-1">Sorting:</label>
+            <template v-for="(order, index) in Object.values(SortingOrder)" :key="index">
+                <div class="relative block">
+                    <input
+                        :id="order"
+                        name="sorting-options"
+                        :value="order"
+                        type="radio"
+                        :checked="vennStore.isSortingOrder(order)"
+                        @change="vennStore.setSortingOrder(order)"
+                    />
+                    <label :for="order" class="ml-1"> {{ order }} </label>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
