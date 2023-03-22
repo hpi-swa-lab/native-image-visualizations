5b2dcb5b4cde5eca64e10c51f4b18cb0dad28758
LucPrestin <l.prestin@gmx.de>
2023-03-22 15:02:01 +0100

    rename filtering options

--- observatory/src/components/controls/FilteringOptions.vue
+++ observatory/src/components/controls/FilteringOptions.vue
@@ -7,38 +7,36 @@ const store = useGlobalStore()
 
 <template>
     <div class="overflow-auto w-full h-[250px] grid grid-cols-8 relative content-start">
-        <p>All |</p>
-        <p>None</p>
-        <div class="col-start-1 w-fit">
-            <template v-for="(filter, index) in store.filters" :key="index">
-                <div class="flex">
-                    <ToggleSwitch
-                        :id="filter.description"
-                        :value="false"
-                        :checked="store.isFilterActive(filter, false)"
-                        role="menuitem"
-                        @change.self="store.toggleFilter(filter, $event)"
-                    />
-                    <ToggleSwitch
-                        :id="filter.description"
-                        :value="true"
-                        :checked="store.isFilterActive(filter, true)"
-                        role="menuitem"
-                        @change.self="store.toggleFilter(filter, $event)"
-                    />
+        <p class="content-center">Only</p>
+        <p class="content-center">None</p>
+        <template v-for="(filter, index) in store.filters" :key="index">
+            <ToggleSwitch
+                class="col-start-1 content-center"
+                :id="filter.description"
+                :value="false"
+                :checked="store.isFilterActive(filter, false)"
+                role="menuitem"
+                @change.self="store.toggleFilter(filter, $event)"
+            />
+            <ToggleSwitch
+                class="content-center"
+                :id="filter.description"
+                :value="true"
+                :checked="store.isFilterActive(filter, true)"
+                role="menuitem"
+                @change.self="store.toggleFilter(filter, $event)"
+            />
 
-                    <p class="whitespace-nowrap flex-1 flex-nowrap" :for="filter.description">
-                        {{ filter.description }}
-                    </p>
-                    <button
-                        v-if="filter.isCustom"
-                        class="btn-sm rounded text-xs btn-danger bg-white sticky right-0 m-1"
-                        @click="() => store.removeFilter(filter)"
-                    >
-                        <font-awesome-icon icon="xmark" />
-                    </button>
-                </div>
-            </template>
-        </div>
+            <p class="col-span-5 whitespace-nowrap flex-1 flex-nowrap" :for="filter.description">
+                {{ filter.description }}
+            </p>
+            <button
+                v-if="filter.isCustom"
+                class="content-center btn-sm rounded text-xs btn-danger bg-white sticky right-0 m-1"
+                @click="() => store.removeFilter(filter)"
+            >
+                <font-awesome-icon icon="xmark" />
+            </button>
+        </template>
     </div>
 </template>
