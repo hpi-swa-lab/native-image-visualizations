895c560065ed193e6650e8cf215da32c54711c71
LucPrestin <l.prestin@gmx.de>
2023-03-29 14:52:36 +0200

    remove unused component and make eslint happy

--- observatory/src/components/help/HelpDialogSankeyTree.vue
+++ observatory/src/components/help/HelpDialogSankeyTree.vue
@@ -4,7 +4,7 @@ const slides = [
     { content: '', src: '2.png' },
     {
         content:
-            "Filtering for a custom nodes selection: Please, note that when filtering for a custom selection, branches where nodes other than the node at the end of a hierarchy level X is selected get filtered out and do not appear on screen.  For example, only when node ci or code (=last node on the branch) from ;jdk.internal.vm.ci;jdk.vm.ci.code is selected, the branch containing this node is filtered in. Selecting any other node here (e.g., ;jdk), filters this branch out. A node's position in the hierarchy is indicated by its path, shown in the tooltip."
+            'Filtering for a custom nodes selection: Please, note that when filtering for a custom selection, branches where nodes other than the node at the end of a hierarchy level X is selected get filtered out and do not appear on screen.  For example, only when node ci or code (=last node on the branch) from ;jdk.internal.vm.ci;jdk.vm.ci.code is selected, the branch containing this node is filtered in. Selecting any other node here (e.g., ;jdk), filters this branch out. A node\'s position in the hierarchy is indicated by its path, shown in the tooltip.'
     }
 ]
 </script>
