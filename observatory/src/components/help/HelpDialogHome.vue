3f03c013f81ac2367fd39a91bb99f66f23c0b9b9
Marcel Garus <github@marcelgarus.dev>
2023-03-29 15:54:48 +0200

    Fix importing of images so it also works in release builds

--- observatory/src/components/help/HelpDialogHome.vue
+++ observatory/src/components/help/HelpDialogHome.vue
@@ -1,8 +1,17 @@
+<script setup lang="ts">
+import img1 from '../../assets/images/home/1.png'
+import img2 from '../../assets/images/home/2.png'
+import img3 from '../../assets/images/home/3.png'
+import img4 from '../../assets/images/home/4.png'
+import img5 from '../../assets/images/home/5.png'
+import img6 from '../../assets/images/home/6.png'
+</script>
+
 <template>
-    <img
-        v-for="index in 6"
-        :key="index"
-        class="border p-4"
-        :src="`src/assets/images/home/${index}.png`"
-    />
+    <img class="border p-4" :src="img1" />
+    <img class="border p-4" :src="img2" />
+    <img class="border p-4" :src="img3" />
+    <img class="border p-4" :src="img4" />
+    <img class="border p-4" :src="img5" />
+    <img class="border p-4" :src="img6" />
 </template>
--- observatory/src/components/help/HelpDialogTreeLine.vue
+++ observatory/src/components/help/HelpDialogTreeLine.vue
@@ -1,8 +1,23 @@
+<script setup lang="ts">
+import img1 from '../../assets/images/treeline/1.png'
+import img2 from '../../assets/images/treeline/2.png'
+import img3 from '../../assets/images/treeline/3.png'
+import img4 from '../../assets/images/treeline/4.png'
+import img5 from '../../assets/images/treeline/5.png'
+import img6 from '../../assets/images/treeline/6.png'
+import img7 from '../../assets/images/treeline/7.png'
+import img8 from '../../assets/images/treeline/8.png'
+import img9 from '../../assets/images/treeline/9.png'
+</script>
+
 <template>
-    <img
-        v-for="index in 9"
-        :key="index"
-        :src="`src/assets/images/treeline/${index}.png`"
-        class="border p-4"
-    />
+    <img class="border p-4" :src="img1" />
+    <img class="border p-4" :src="img2" />
+    <img class="border p-4" :src="img3" />
+    <img class="border p-4" :src="img4" />
+    <img class="border p-4" :src="img5" />
+    <img class="border p-4" :src="img6" />
+    <img class="border p-4" :src="img7" />
+    <img class="border p-4" :src="img8" />
+    <img class="border p-4" :src="img9" />
 </template>
